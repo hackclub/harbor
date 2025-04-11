0beaada769de224d62893a69ad38b49ed3ec2d29
Max Wofford <max@maxwofford.com>
2025-04-11 14:20:33 -0400

    Rewrite sailors log again

--- app/jobs/sailors_log_poll_for_changes_job.rb
+++ app/jobs/sailors_log_poll_for_changes_job.rb
@@ -3,33 +3,37 @@ class SailorsLogPollForChangesJob < ApplicationJob
 
   def perform
     puts "performing SailorsLogPollForChangesJob"
-    users_who_coded = Heartbeat.where("created_at > ?", 1.minutes.ago)
-                               .where(time: 1.minutes.ago..)
+    users_who_coded = Heartbeat.with_valid_timestamps
+                               .where(time: 10.minutes.ago..)
                                .distinct.pluck(:user_id)
     puts "users_who_coded: #{users_who_coded}"
 
-    new_notifs = SailorsLog.includes(:user, :notification_preference)
-                           .where(notification_preference: { enabled: true })
-                           .where(user_id: users_who_coded)
-                           .each(&:update_sailors_log)
-    
-    # create notifications
-    # background job to notify user
-    notifs_to_send = Heartbeat.insert_all(new_notifs)
+    slack_uids = User.where(id: users_who_coded).pluck(:slack_uid)
+    puts "slack_uids: #{slack_uids}"
 
-    notifs_to_send.map(&:notify_user_later!)
+    new_notifs = SailorsLog.includes(:user, :notification_preferences)
+                           .where(notification_preferences: { enabled: true })
+                           .where(slack_uid: slack_uids)
+                           .map { |sl| update_sailors_log(sl) }.flatten
+
+    notifs_to_send = SailorsLogSlackNotification.insert_all(new_notifs)
+    notif_ids = notifs_to_send.result.to_a.map { |r| r["id"] }
+
+    SailorsLogSlackNotification.where(id: notif_ids).map(&:notify_user_later!)
   end
 
   private
 
   def update_sailors_log(sailors_log)
-    project_durations = sailors_log.user.duration_seconds.group(:project)
     project_updates = []
+    project_durations = Heartbeat.where(user_id: sailors_log.user.id)
+                                 .group(:project).duration_seconds
     project_durations.each do |k, v|
-      # TODO: check if project duration is in hours or seconds
-      old_duration = sailors_log.projects_summary[k]
-      new_duration = v / 3600
-      if (old_duration < new_duration)
+      old_duration = sailors_log.projects_summary[k] || 0
+      new_duration = v
+      puts "#{k}| old_duration: #{old_duration}, new_duration: #{new_duration}"
+      if old_duration / 3600 < new_duration / 3600
+        puts "updating #{k} to #{new_duration}"
         sailors_log.projects_summary[k] = new_duration
         project_updates << { project: k, duration: new_duration }
       end
@@ -37,19 +41,19 @@ class SailorsLogPollForChangesJob < ApplicationJob
 
     notifications_to_create = []
     if sailors_log.changed?
-      sailors_log.save!
-      # for each notification_preference, for each project_update
-      sailors_log.notification_preference.each do |np|
+      sailors_log.notification_preferences.each do |np|
         project_updates.map do |pu|
+          puts "np: #{np.inspect}, pu: #{pu.inspect}"
           notifications_to_create << {
             slack_uid: sailors_log.user.slack_uid,
             slack_channel_id: np.slack_channel_id,
-            project_name: pu.project,
-            project_duration: pu.duration
+            project_name: pu[:project],
+            project_duration: pu[:duration]
           }
         end
       end
 
+      sailors_log.save!
     end
 
     notifications_to_create
--- app/models/sailors_log.rb
+++ app/models/sailors_log.rb
@@ -1,8 +1,8 @@
 class SailorsLog < ApplicationRecord
   validates :slack_uid, presence: true, uniqueness: true
-  validates :projects_summary, presence: true
 
   before_validation :initialize_projects_summary
+  after_create :ensure_notification_preference_for_debug_channel
 
   belongs_to :user, foreign_key: :slack_uid, primary_key: :slack_uid
 
@@ -19,7 +19,14 @@ class SailorsLog < ApplicationRecord
   private
 
   def initialize_projects_summary
-    return unless projects_summary.blank?
-    self.projects_summary = Heartbeat.where(user_id: slack_uid).group(:project).duration_seconds
+    return if projects_summary.present?
+    self.projects_summary = Heartbeat.where(user_id: user.id)
+                                     .group(:project).duration_seconds
+    self.projects_summary ||= {}
+  end
+
+  def ensure_notification_preference_for_debug_channel
+    return if notification_preferences.any? { |np| np.slack_channel_id == "C0835AZP9GB" && np.enabled }
+    notification_preferences.create!(slack_channel_id: "C0835AZP9GB", enabled: true)
   end
 end

267f15abbd8d7b2d22a6607809ca5c219515ca6e
Ethan Canterbury <ecanterbury@hacksomerset.org>
2025-04-21 01:56:34 +0000

    Fixed spacing

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -8,11 +8,11 @@ class Api::V1::UserController < ApplicationController
   private
 
   def authenticate_api_key
-    api_key = request.headers['Authorization']&.split(' ')&.last
-    return render json: { error: 'Unauthorized' }, status: :unauthorized unless api_key
+    api_key = request.headers["Authorization"]&.split(" ")&.last
+    return render json: { error: "Unauthorized" }, status: :unauthorized unless api_key
 
     api_key_record = ApiKey.find_by(token: api_key)
-    return render json: { error: 'Unauthorized' }, status: :unauthorized unless api_key_record
+    return render json: { error: "Unauthorized" }, status: :unauthorized unless api_key_record
 
     @current_user = api_key_record.user
   end

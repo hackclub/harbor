94302dc99e37d444156f0b6849413b751d3c8f97
Max Wofford <max@maxwofford.com>
2025-04-25 13:24:22 -0400

    Switch to Open3 for running git ls-remote

--- lib/git_remote.rb
+++ lib/git_remote.rb
@@ -1,6 +1,8 @@
+require "open3"
+
 class GitRemote
   def self.check_remote_exists(repo_url)
-    `git ls-remote #{repo_url}`
-    $?.success?
+    safe_repo_url = URI.parse(repo_url).to_s.gsub(" ", "").gsub("'", "")
+    Open3.capture2e("git", "ls-remote", safe_repo_url).last.success?
   end
 end

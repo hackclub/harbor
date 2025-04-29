fa49d73d29651a0925807bcf9fae96f34f89a742
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-04-29 22:27:44 +0200

    some more work

--- app/jobs/one_time/migrate_wakatimecom_heartbeats_job.rb
+++ app/jobs/one_time/migrate_wakatimecom_heartbeats_job.rb
@@ -37,42 +37,46 @@ class OneTime::MigrateWakatimecomHeartbeatsJob < ApplicationJob
       end
     end
 
-    output_dir = Rails.root.join('storage', 'wakatime_dumps')
+    output_dir = Rails.root.join("storage", "wakatime_dumps")
     FileUtils.mkdir_p(output_dir)
     output_path = output_dir.join("wakatime_heartbeats_#{@user.id}.json")
 
     puts "downloading wakatime.com heartbeats dump for user #{@user.id}"
-    auth_token = Base64.strict_encode64("#{@user.wakatime_api_key}:")
-    
-    File.open(output_path, 'wb') do |file|
+    File.open(output_path, "wb") do |file|
       # i don't get why with HTTP it doesn't work...
-      file.write(URI.open(dump['download_url']).read)
+      file.write(URI.open(dump["download_url"]).read)
     end
-    
+
     puts "wakatime.com heartbeats saved to #{output_path} for user #{@user.id}"
+
+    heartbeats = Heartbeat.where(user_id: @user.id)
+    # this could explode, let's see how it ends up.
+    parsed_json = JSON.parse(File.read(output_path))
+
+    # the json has a schema of:
   end
 
   def get_dumps
     auth_token = Base64.strict_encode64("#{@user.wakatime_api_key}:")
     response = HTTP.auth("Basic #{auth_token}")
-      .get('https://api.wakatime.com/api/v1/users/current/data_dumps')
+      .get("https://api.wakatime.com/api/v1/users/current/data_dumps")
 
     if response.status.success?
-      dumps = JSON.parse(response.body)['data'].find { |dump| dump['type'] == 'heartbeats' && dump['status'] == 'Completed' }
-      return dumps || {}
+      dumps = JSON.parse(response.body)["data"].find { |dump| dump["type"] == "heartbeats" && dump["status"] == "Completed" }
+      dumps || {}
     else
       puts "Failed to fetch Wakatime.com data dumps: #{response.status} - #{response.body}"
-      return {}
+      {}
     end
   end
 
   def create_dump
     auth_token = Base64.strict_encode64("#{@user.wakatime_api_key}:")
-    response = HTTP.auth("Basic #{auth_token}")
-      .post('https://api.wakatime.com/api/v1/users/current/data_dumps',
+    HTTP.auth("Basic #{auth_token}")
+      .post("https://api.wakatime.com/api/v1/users/current/data_dumps",
         json: {
-          type: 'heartbeats',
-          email_when_finished: false,
+          type: "heartbeats",
+          email_when_finished: false
         }
       )
   end
